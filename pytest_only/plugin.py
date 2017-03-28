deaaffa8d93b3fe0407d1b229eef83e1d55c56ad
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2017-03-28 07:20:50 -0400

    invoke pytest_deselected with deselected items
    Fixes #1

--- pytest_only/plugin.py
+++ pytest_only/plugin.py
@@ -1,4 +1,10 @@
 def pytest_collection_modifyitems(config, items):
-    only = [item for item in items if item.get_marker('only')]
+    only, other = [], []
+    for item in items:
+        l = only if item.get_marker('only') else other
+        l.append(item)
+
     if only:
         items[:] = only
+        if other:
+            config.hook.pytest_deselected(items=other)
