8eea4d442cbd8d71331e9ba175e4c59c8d09051e
Zach Kanzler <they4kman@gmail.com>
2024-03-08 16:08:54 -0500

    chore(ops): expand tox support to py3.12 and pytest 7.2 to 8.0
    At present, 8.1 is yanked from PyPI, and no suitable replacement yet pushed

--- _tox_install_command.sh
+++ _tox_install_command.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Virtualenvs created in 3.12+ may malfunction with older setuptools
+pip install -U setuptools
+
 poetry install -v \
  && poetry run pip install --no-warn-conflicts "$@"
